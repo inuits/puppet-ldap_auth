c71fd9f306a38ea45b7b2d446d2ee23b2aab523f
flyer <tom.decooman@gmail.com>
2012-06-11 15:15:29 +0200

    unless

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,7 +36,7 @@ class ldap_auth ( $server="localhost", $base, $binddn, $bindpw, $filter ) {
 			exec { "authconfig --enableldap --enableldapauth --ldapserver=$server --ldapbasedn=$base --enablemkhomedir --update":
 				path => ["/usr/bin", "/usr/sbin"],
 				require => Package[$ldap_auth::params::packages],
-				onlyif => "/bin/grep 'dc=example,dc=com' /etc/nslcd.conf";
+				unless => "/bin/grep 'ldap' /etc/nsswitch.conf";
 			}
     }
 	}
