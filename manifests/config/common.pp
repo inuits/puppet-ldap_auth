c9dcde112c4586f2b4a215251bbd0321dcffabf2
Dave Simons <dave@inuits.eu>
2012-07-06 17:41:37 +0200

    set group entries in nsswitch.conf to use ldap as well
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/config/common.pp
+++ manifests/config/common.pp
@@ -5,6 +5,7 @@ class ldap_auth::config::common {
     changes => [
       "set /files/etc/nsswitch.conf/database[. = 'passwd']/service[2] ldap",
       "set /files/etc/nsswitch.conf/database[. = 'shadow']/service[2] ldap",
+      "set /files/etc/nsswitch.conf/database[. = 'group' ]/service[2] ldap",
     ]
   }
 
