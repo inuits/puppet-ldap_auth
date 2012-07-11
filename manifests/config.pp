6182a6c5773ff4be53e9748da9312c7ade1faa13
Dave Simons <dave@inuits.eu>
2012-07-11 14:49:11 +0200

    add augeas stuff for pam_mkhomedir support on debian
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,7 @@ class ldap_auth::config {
 
   case $::operatingsystem {
     /(?i:centos|redhat|fedora)/: { include ldap_auth::config::redhat }
+    /(?i:debian|ubuntu)/:        { include ldap_auth::config::debian }
     default:                     {}
   }
 
---
+++ manifests/config/debian.pp
@@ -0,0 +1,14 @@
+class ldap_auth::config::debian {
+
+  augeas{'pam_mkhomedir.so':
+    context => '/files/etc/pam.d/common-session/100',
+    changes => [
+      'set type session',
+      'set control required',
+      'set module pam_mkhomedir.so',
+      'set argument[1] umask=0022',
+    ],
+    onlyif => 'get /files/etc/pam.d/common-session/*[module = "pam_mkhomedir.so"]/type != session',
+  }
+
+}
