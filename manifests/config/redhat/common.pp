f51e2c33f6d444fdc9309a948c633baca343bc26
Dave Simons <dave@inuits.eu>
2012-07-12 16:07:25 +0200

    only notify nslcd on rhel6.x
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/config/redhat/common.pp
+++ manifests/config/redhat/common.pp
@@ -4,7 +4,10 @@ class ldap_auth::config::redhat::common {
     path    => ["/usr/bin", "/usr/sbin"],
     require => Package[$::ldap_auth::params::_packages],
     unless  => '/bin/grep "ldap" /etc/nsswitch.conf',
-    notify  => Service[$::ldap_auth::params::_nslcd_service],
+    notify  => $::operatingsystemrelease ? {
+      /^6/ => Service[$::ldap_auth::params::_nslcd_service],
+      /^5/ => undef,
+    },
     before  => Augeas['nsswitch.conf'],
   }
 
