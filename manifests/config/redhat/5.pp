7c6561664fadea1ffbce2ac5c8fb5cb94d02f22b
Dave Simons <dave@inuits.eu>
2012-07-12 12:49:16 +0200

    add redhat 5.x support
    Signed-off-by: Dave Simons <dave@inuits.eu>

---
+++ manifests/config/redhat/5.pp
@@ -0,0 +1,22 @@
+class ldap_auth::config::redhat::5 {
+
+  File{
+    owner => 'root',
+    group => 'root',
+  }
+
+  file{'/etc/ldap.conf':
+    mode    => '0644',
+    content => template('ldap_auth/ldap.conf.erb'),
+  }
+
+  file{'/etc/ldap.secret':
+    mode    => '0600',
+    content => "$ldap_auth::params::_bindpw\n",
+  }
+
+  service{'nscd':
+    ensure => 'stopped',
+  }
+
+}
---
+++ manifests/config/redhat/6.pp
@@ -0,0 +1,30 @@
+class ldap_auth::config::redhat::6 {
+
+  file{'/etc/nslcd.conf':
+    owner   => 'root',
+    group   => 'nslcd',
+    mode    => '0640',
+    content => template('ldap_auth/nslcd.conf.erb'),
+    require => Package[$::ldap_auth::params::_packages],
+    notify  => Service[$::ldap_auth::params::_nslcd_service],
+  }
+
+  service{$::ldap_auth::params::_nslcd_service:
+    ensure  => 'running',
+    require => File["/etc/nslcd.conf"],
+  }
+
+  exec{"authconfig --enableldap --enableldapauth --ldapserver=$::ldap_auth::params::_server --ldapbasedn=$::ldap_auth::params::_base --enablemkhomedir --update":
+    path    => ["/usr/bin", "/usr/sbin"],
+    require => Package[$::ldap_auth::params::_packages],
+    unless  => '/bin/grep "ldap" /etc/nsswitch.conf',
+    notify  => Service[$::ldap_auth::params::_nslcd_service],
+    before  => Augeas['nsswitch.conf'],
+  }
+
+  group{'nslcd':
+    ensure => 'present',
+    gid    => '7050',
+  }
+
+}
