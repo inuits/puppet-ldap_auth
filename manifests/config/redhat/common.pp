18d3e3c50ea7de1d6ab507d42d1cfa855a6d433e
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-05-10 16:37:01 +0200

    Manage some ldap options in authconfig with augeas on Redhat based system
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/config/redhat/common.pp
+++ manifests/config/redhat/common.pp
@@ -5,11 +5,19 @@ class ldap_auth::config::redhat::common {
     path    => ['/usr/bin', '/usr/sbin'],
     require => Package[$::ldap_auth::params::_packages],
     unless  => '/bin/grep "ldap" /etc/nsswitch.conf',
-    before  => Augeas['nsswitch.conf'],
+    before  => [ Augeas['nsswitch.conf'], Augeas['authconfig'] ],
     notify  => operatingsystemrelease ? {
       /^6/    => Service[$::ldap_auth::params::_nslcd_service],
       default => undef,
     },
   }
 
+  augeas{'authconfig':
+    context => '/files/etc/authconfig',
+    changes => [
+      'set /files/etc/sysconfig/authconfig/USELDAPAUTH yes',
+      'set /files/etc/sysconfig/authconfig/USELDAP yes',
+      'set /files/etc/sysconfig/authconfig/USEMKHOMEDIR yes',
+    ]
+  }
 }
